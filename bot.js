0d7121dfae9b7a9fca2557e1e3f3728bb0d26ed2
Florence Tat <lacefronte@gmail.com>
2025-03-18 22:15:50 -0230

    Reinstated the embed object

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits, EmbedBuilder } from 'discord.js';
 import { config } from 'dotenv';
 
 // loads process.env contents
@@ -20,17 +20,31 @@ client.once(Events.ClientReady, readyDiscord);
 client.login(process.env.TOKEN);
 
 
+
 // Finds Discord channel ID to post the Astronomy Photo of the Day in a readable format
 client.on('ready', async () => {
 
     var testChannel = client.channels.cache.find(channel => channel.id === process.env.CHANNELID);
     const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.APIKEY}`)
     const data = await response.json();
+
+    // Embed object for the NASA APOD post
+    const apodEmbed = new EmbedBuilder()
+        .setColor(`Blurple`)
+        .setTitle(`${data.title}`)
+        .setURL(`https://apod.nasa.gov/apod/astropix.html`)
+        .setAuthor({ name: `NASA`, iconURL:`https://i.imgur.com/S4oQ4OT.png`, url: `https://www.nasa.gov/`})
+        .setDescription(`${data.explanation}`)
+        .setThumbnail(`https://i.imgur.com/S4oQ4OT.png`)
+        .addFields( { name: `Date`, value: `${data.date}` })
+        .setImage(`${data.url}`)
+        .setTimestamp()
+        .setFooter({ text: `${data.copyright}`, iconURL: `https://i.imgur.com/S4oQ4OT.png`});
     
     // 86400000 milliseconds = 24 hours
     try {
         setInterval( () => {
-        testChannel.send(`## **${data.title}** \n**Credit:** ${data.copyright} \n${data.date} \n\n*${data.explanation}*\n${data.url}`);
+        testChannel.send({ embeds: [apodEmbed] });
     }, 86400000);}
 
     catch(err) {
